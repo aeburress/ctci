7740b4f4c13ca44707c3e1d5c60e7e514072b123
aeburress <aeburress@gmail.com>
2017-03-12 18:07:07 +0800

    Implement Ex1.2 in O(n) time with tests

--- Chap01/Ex1.java
+++ Chap01/Ex1.java
@@ -1,7 +1,9 @@
 import java.util.Set;
 import java.util.HashSet;
+import java.util.Arrays;
 
 public class Ex1 {
+  // Exercise 1.1 --------------------------------------------------------------
 
   /** isUnique() checks whether a String has any repeating characters. It is
       case sensitive. It does this by copying each character from the String
@@ -33,5 +35,47 @@ public class Ex1 {
     else {
       return false;
     }
+  }
+
+  // Exercise 1.2 --------------------------------------------------------------
+  public boolean isPermutation(String s1, String s2) {
+
+    // Uninitialised string check
+    if (s1 == null || s2 == null) {
+      throw new NullPointerException();
+    }
+
+    // Empty string check
+    if (s1.length() == 0 || s2.length() == 0) {
+      throw new IllegalArgumentException("Empty String/s passed to isPermutation()");
+    }
+
+    // identical string check
+    if (s1.equals(s2)) {
+      return false;
+    }
+
+    int[] asciiCounts1 = getAsciiCounts(s1);
+    int[] asciiCounts2 = getAsciiCounts(s2);
+
+    if (Arrays.equals(asciiCounts1, asciiCounts2)) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  private int[] getAsciiCounts(String s) {
+
+    int[] counts = new int[128];
+    int asciiCode;
+
+    for (int ii = 0; ii < s.length(); ii++) {
+      asciiCode = (int)s.charAt(ii);
+      counts[asciiCode]++;
+    }
+
+    return counts;
   }
 }
--- Chap01/Ex1Test.java
+++ Chap01/Ex1Test.java
@@ -1,6 +1,5 @@
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.Rule;
 import org.junit.rules.ExpectedException;
@@ -8,6 +7,7 @@ import org.hamcrest.Matcher;
 
 public class Ex1Test {
 
+  // Exercise 1.1 --------------------------------------------------------------
   @Test
   public void testNormalInputIsUnique() {
     Ex1 ex1 = new Ex1();
@@ -38,5 +38,76 @@ public class Ex1Test {
     thrown.expect(IllegalArgumentException.class);
     //thrown.expectMessage("");
     ex1.isUnique("");
+  }
+
+  // Exercise 1.2 --------------------------------------------------------------
+  @Test
+  public void testNormalInputIsPermutation() {
+    Ex1 ex1 = new Ex1();
+    assertTrue("expected true", ex1.isPermutation("banana", "nabana"));
+    assertFalse("expected false", ex1.isPermutation("12345", "12346"));
+    assertFalse("expected false", ex1.isPermutation("acted", "act"));
+    assertFalse("expected false", ex1.isPermutation("out", "feast"));
+    assertFalse("expected false", ex1.isPermutation("1234555", "1112345"));
+    assertFalse("expected false", ex1.isPermutation("equal", "equal"));
+    assertTrue("expected true", ex1.isPermutation("qwertyuiopasdfghjklzxcvbnm", "mnbvcxzlkjhgfdsapoiuytrewq"));
+    assertFalse("expected false", ex1.isPermutation("a", "b"));
+    assertFalse("expected false", ex1.isPermutation("aaaaa", "aaa"));
+  }
+
+  @Test
+  public void testTwoEmptyStringsIsPermutation() {
+    Ex1 ex1 = new Ex1();
+
+    thrown.expect(IllegalArgumentException.class);
+    ex1.isPermutation("", "");
+  }
+
+  @Test
+  public void testFirstEmptyStringsIsPermutation() {
+    Ex1 ex1 = new Ex1();
+
+    thrown.expect(IllegalArgumentException.class);
+    ex1.isPermutation("", "foo");
+  }
+
+  @Test
+  public void testSecondEmptyStringsIsPermutation() {
+    Ex1 ex1 = new Ex1();
+
+    thrown.expect(IllegalArgumentException.class);
+    ex1.isPermutation("bar", "");
+  }
+
+  @Test
+  public void testTwoNullsIsPermutation() {
+    Ex1 ex1 = new Ex1();
+
+    thrown.expect(NullPointerException.class);
+    ex1.isPermutation(null, null);
+  }
+
+  @Test
+  public void testFirstNullsIsPermutation() {
+    Ex1 ex1 = new Ex1();
+
+    thrown.expect(NullPointerException.class);
+    ex1.isPermutation(null, "bar");
+  }
+
+  @Test
+  public void testSecondNullsIsPermutation() {
+    Ex1 ex1 = new Ex1();
+
+    thrown.expect(NullPointerException.class);
+    ex1.isPermutation(null, "bar");
+  }
+
+  @Test
+  public void testNullsWithEmptyStringIsPermutation() {
+    Ex1 ex1 = new Ex1();
+
+    thrown.expect(NullPointerException.class);
+    ex1.isPermutation(null, "");
   }
 }
